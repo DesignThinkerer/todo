5a6f72c166e52e3b64a1a279defd680d9746d0ab
Théophile Desmedt <34781168+DesignThinkerer@users.noreply.github.com>
2024-12-07 17:23:27 +0100

    add missing extensions to files

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import Task from "./Task"
-import TaskManager from "./TaskManager"
-import TaskApp from "./TaskApp"
+import Task from "./Task.js"
+import TaskManager from "./TaskManager.js"
+import TaskApp from "./TaskApp.js"
 
 document.addEventListener("DOMContentLoaded", () => {
     const taskManager = new TaskManager();
