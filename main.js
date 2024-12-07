eb464519c2de446a4b964ead0bd6dd04ce780df2
Théophile Desmedt <34781168+DesignThinkerer@users.noreply.github.com>
2024-12-07 17:19:04 +0100

    init app

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ import Task from "./Task"
 import TaskManager from "./TaskManager"
 import TaskApp from "./TaskApp"
 
+document.addEventListener("DOMContentLoaded", () => {
+    const taskManager = new TaskManager();
+    new TaskApp(taskManager, "#task-app");
+  });  
